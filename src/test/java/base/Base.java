878608e12c0ccce0b3ea7f65876d44cc86422a5f
kamkom84 <kbd.dimitrov@gmail.com>
2024-07-18 23:37:12 +0300

    changes

--- src/test/java/base/Base.java
+++ src/test/java/base/Base.java
@@ -69,9 +69,9 @@ public class Base {
 		options.addArguments("--disable-gpu");
 		options.addArguments("--window-size=1920,1080");
 
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utilities.IMPLICIT_WAIT_TIME));
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Utilities.PAGE_LOAD_TIME));
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Utilities.IMPLICIT_WAIT_TIME));
+//		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(Utilities.PAGE_LOAD_TIME));
 		driver.get(prop.getProperty("url"));
 		
 		return driver;

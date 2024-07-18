7bce1856da49b667dd36cfcfa06ed61e295c4889
kamkom84 <kbd.dimitrov@gmail.com>
2024-07-18 19:44:00 +0300

    d "firefoxxx"

--- src/test/java/base/Base.java
+++ src/test/java/base/Base.java
@@ -47,7 +47,7 @@ public class Base {
 			
 			driver = new ChromeDriver();
 			
-		}else if(browser.equalsIgnoreCase("firefox")) {
+		}else if(browser.equalsIgnoreCase("firefoxxx")) {
 			
 			driver = new FirefoxDriver();
 			
